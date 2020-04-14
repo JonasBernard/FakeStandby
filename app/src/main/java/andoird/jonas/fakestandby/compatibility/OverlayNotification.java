9239b324f5e6e4cedb52e6ce4d5157f0442a6017
Jonas <pass.jbernard@web.de>
2020-04-14 17:27:11 +0200

    Migirating to Android X

--- app/src/main/java/andoird/jonas/fakestandby/compatibility/OverlayNotification.java
+++ app/src/main/java/andoird/jonas/fakestandby/compatibility/OverlayNotification.java
@@ -3,8 +3,8 @@ package andoird.jonas.fakestandby.compatibility;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.NotificationManagerCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import andoird.jonas.fakestandby.R;
 import andoird.jonas.fakestandby.service.AccessibilityOverlayService;
